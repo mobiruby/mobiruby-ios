55d611e6b00aefed541667f450c86dfd50b846c2
Yuichiro MASUI <masui@masuidrive.jp>
2012-09-10 14:32:24 +0900

    Initial Commit

---
+++ mobiruby-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mobiruby-ios
+//
+//  Created by Yuichiro MASUI on 9/10/12.
+//  Copyright (c) 2012 MobiRuby Developers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
