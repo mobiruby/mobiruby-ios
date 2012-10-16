719e08a5f023fde5185f9827bd532063e1919cf9
Yuichiro MASUI <masui@masuidrive.jp>
2012-10-17 03:08:37 +0900

    Updated mruby

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
+git pull origin master
 git submodule init
+(cd modules/mruby/ && git checkout include/mrbconf.h)
+git submodule foreach 'git pull origin master'
 git submodule update
+sed -i -e s/typedef\ int\ mrb_int/typedef\ long\ mrb_int/g modules/mruby/include/mrbconf.h
+sed -i -e s/define\ MRB_INT_MIN\ INT_MIN/define\ MRB_INT_MIN\ LONG_MIN/g modules/mruby/include/mrbconf.h
+sed -i -e s/define\ MRB_INT_MAX\ INT_MAX/define\ MRB_INT_MAX\ LONG_MAX/g modules/mruby/include/mrbconf.h
+
 sh ./bin/build-libffi.sh
 sh ./bin/build-mrbc.sh
