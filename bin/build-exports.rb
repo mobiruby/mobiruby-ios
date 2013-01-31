5afba8d1e258d57d414b2a0d17e7e1d27c26b77d
Yuichiro MASUI <masui@masuidrive.jp>
2013-02-01 02:19:44 +0900

    Fixed bridgesupport

--- bin/build-exports.rb
+++ bin/build-exports.rb
@@ -6,29 +6,9 @@ require 'nokogiri'
 gem 'xcodeproj', '0.3.5'
 require 'xcodeproj'
 
-VERSIONS = {
-  50000 => '5.0',
-  50100 => '5.1',
-  60100 => '5.0',
-  60100 => '6.0',
-  60100 => '6.1',
-  60200 => '6.2',
-  70000 => '7.0',
-}
-SDKROOT = ARGV[2] || ENV["SDKROOT"]
-def sdkroot(version)
-  File.join(File.dirname(SDKROOT), File.basename(SDKROOT).sub(/\d.*/, '')+VERSIONS[version]+".sdk")
-end
-
-VERSIONS.delete_if do |version, s|
-  !File.exists?(sdkroot(version))
-end
-
-LATEST_VERSION = VERSIONS.keys.max
-
 OUTPUT_FILE = ARGV[0]
 PROJECT_FILE_PATH = ARGV[1] || ENV['PROJECT_FILE_PATH']
-
+SDKROOT = ARGV[2] || ENV["SDKROOT"]
 CFLAGS = ARGV[3] || ENV["OTHER_CFLAGS"] +
   " -D__IPHONE_OS_VERSION_MIN_REQUIRED=#{ENV['PLATFORM_VERSION_AVAILABILITY_H_FORMAT']}"
 
@@ -51,7 +31,8 @@ class BridgeMetadata
     end
 
     doc.xpath('//constant').each do |c|
-      @consts[c['name']] = '    {.name="%s", .type="%s", .value=(void*)&%s}' % [c['name'].gsub(/^\w/) { |s| s.upcase }, c['type'], c['name']]
+     # @consts[c['name']] = '    {.name="%s", .type="%s", .value=(void*)&%s}' % [c['name'].gsub(/^\w/) { |s| s.upcase }, c['type'], c['name']]
+      @consts[c['name']] = '    {.name="%s", .type="%s", .value=(void*)"%s"}' % [c['name'], c['type'], c['name']]
     end
 
     doc.xpath('//enum').each do |e|
@@ -100,21 +81,17 @@ pr = Xcodeproj::Project.new(PROJECT_FILE_PATH)
 frameworks = pr.groups.where(:name => 'Frameworks').children #.map{|a| File.join(SDKROOT, a.path)}
 
 imports = []
-metadata = {}
-VERSIONS.each do |version, version_str|
-  metadata[version] = BridgeMetadata.new
+metadata = BridgeMetadata.new
 
-  root = sdkroot(version)
-  frameworks.each do |fw|
-    command = %Q[/usr/bin/gen_bridge_metadata --no-64-bit -f "#{File.join(root, fw.path)}" -c "#{CFLAGS.gsub('"', '\\"')} -isysroot "#{root}" -framework #{fw.name.gsub('.framework', '')}"]
+frameworks.each do |fw|
+  command = %Q[/usr/bin/gen_bridge_metadata --no-64-bit -f "#{File.join(SDKROOT, fw.path)}" -c "#{CFLAGS.gsub('"', '\\"')} -isysroot "#{SDKROOT}" -framework #{fw.name.gsub('.framework', '')}"]
 
-    xml = open("|#{command}").read
-    imports += Dir.glob(File.join(root, fw.path, 'Headers', '*.h')).map do |header|
-      header.gsub(File.join(root, fw.path, 'Headers'), fw.name.gsub('.framework', ''))
-    end
-
-    metadata[version].parse(xml)
+  xml = open("|#{command}").read
+  imports += Dir.glob(File.join(SDKROOT, fw.path, 'Headers', '*.h')).map do |header|
+    header.gsub(File.join(SDKROOT, fw.path, 'Headers'), fw.name.gsub('.framework', ''))
   end
+
+  metadata.parse(xml)
 end
 
 
@@ -142,66 +119,27 @@ open(OUTPUT_FILE, 'w').puts <<__STR__
 */
 #include "cocoa.h"
 #include "mruby/value.h"
+#include <dlfcn.h>
 
 #{imports.uniq.map{|i| "#import \"#{i}\""}.join("\n")}
 
 
-#{metadata[LATEST_VERSION].c_structs(:structs_table, :static)}
+#{metadata.c_structs(:structs_table, :static)}
 
-#{metadata[LATEST_VERSION].c_enums(:enums_table, :static)}
+#{metadata.c_enums(:enums_table, :static)}
 
-#{
-  VERSIONS.keys.sort.map { |version|
-    vs = metadata[version].consts.length
-    if LATEST_VERSION != version
-      (metadata[version].consts.keys - metadata[LATEST_VERSION].consts.keys).each do |key|
-        metadata[version].consts.delete key
-      end
-    end
-    "static struct BridgeSupportConstTable* consts_table_%d() {\n%s\n  return consts_table;\n}" % [
-      version,
-      metadata[version].c_consts("consts_table", "    ")
-    ]
-  }.join("\n")
-}
-
-static int getSystemVersionAsAnInteger()
-{
-    int index = 0;
-    NSInteger version = 0;
-
-    NSArray* digits = [[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."];
-    NSEnumerator* enumer = [digits objectEnumerator];
-    NSString* number;
-    while (number = [enumer nextObject]) {
-        if (index>2) {
-            break;
-        }
-        NSInteger multipler = powf(100, 2-index);
-        version += [number intValue]*multipler;
-        index++;
-    }
-  return version;
-}
+#{metadata.c_consts(:consts_table, :static)}
 
 void
 init_cocoa_bridgesupport(mrb_state *mrb)
 {
-    static struct BridgeSupportConstTable *consts_table = NULL;
-    if(consts_table == NULL) {
-        consts_table = consts_table_#{LATEST_VERSION}();
-        int ios_ver = getSystemVersionAsAnInteger();
-
-        #{
-VERSIONS.keys.sort.reverse.map do |version|
-  "if(ios_ver >= #{version}) { consts_table = consts_table_#{version}(); }"
-end.join("\n        else ")
-}
-        else {
-            NSLog(@"don't support this version");
-        }
-        NSLog(@"consts_table=%p", consts_table);
+    void *dlh = dlopen(NULL, RTLD_LAZY);
+    struct BridgeSupportConstTable *ccur = consts_table;
+    while(ccur->name) {
+        ccur->value = dlsym(dlh, (const char*)ccur->value);
+        ++ccur;
     }
+    dlclose(dlh);
     load_cocoa_bridgesupport(mrb, structs_table, consts_table, enums_table);
 }
 
